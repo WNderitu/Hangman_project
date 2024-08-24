2d97ffb9d288d770c409973e472cdccfa0c36fd8
WNderitu <hildah.nderituh@gmail.com>
2024-08-24 20:30:53 +0300

    Choice of random words

---
+++ Hangman_proj.py
@@ -0,0 +1,7 @@
+import random
+
+hgmn_rdm_wrds = ['Rhythm', 'Avenue', 'Luxury', 'Subway', 'Uptown', 'Gossip','Vortex', 'Bikini','Cobweb']
+
+guess_wrd = random.choices(hgmn_rdm_wrds)
+print(guess_wrd)
+
